ca0e1f35df75e1ea97fee6691690422493cd7786
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-10-03 12:53:46 +0200

    I completed function to change sellin and quality

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace GildedRose
 {
@@ -20,10 +23,57 @@ namespace GildedRose
         /// "Backstage passes to a TAFKAL80ETC concert"
         /// "Conjured Mana Cake"
         /// </summary>
+         
         public void UpdateQuality()
         {
-            // TODO ...
             // Hint: Iterate through this.items and check Name property to access specific item
+            foreach(Item item in items)
+            {
+                if(!item.Name.Contains("Sulfuras"))
+                {
+                    item.SellIn--;
+                    if (item.Name.Contains("Conjured"))
+                    {
+                        if (item.Quality > 0)
+                            item.Quality = item.SellIn >= 0 ? item.Quality - 2 : item.Quality - 4;
+
+                    }
+                    else if (item.Name.Contains("Aged Brie"))
+                    {
+                        if (item.Quality < 50)
+                            item.Quality = item.SellIn >= 0 ? item.Quality + 1 : item.Quality + 2;
+                    }
+                    else if (item.Name.Contains("Backstage passes"))
+                    {
+                        if (item.SellIn <= 10 && item.SellIn > 5)
+                            item.Quality += 2;
+
+                        if (item.SellIn <= 5)
+                            item.Quality += 3;
+
+                        if (item.SellIn < 0)
+                            item.Quality = 0;
+
+                        else if(item.SellIn > 10)
+                            item.Quality++;
+
+                    }
+                    else
+                    {
+                        //Normal items
+                        if(item.Quality > 0)
+                            item.Quality = item.SellIn >= 0 ? item.Quality - 1 : item.Quality - 2;
+
+                    }
+                    //Protection. Quality can be between 0 - 50 expect legendary items
+                    if (item.Quality < 0)
+                        item.Quality = 0;
+
+                    if (item.Quality > 50)
+                        item.Quality = 50;
+                }
+            }
+            
         }
     }
 }
